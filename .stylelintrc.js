f98be5ec92d2cb9afb348444abebe84e50e2df2a
Arctic Ice Studio <development@arcticicestudio.com>
2019-12-20 11:22:40 +0100

    Nord Docs Transition
    All documentations and assets have been moved to the official Nord
    website and documentations [1,2].
    
    [1]: https://www.nordtheme.com/ports/highlightjs
    [2]: https://github.com/arcticicestudio/nord-docs
    
    GH-13

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 2017-present Arctic Ice Studio <development@arcticicestudio.com>
- * Copyright (c) 2017-present Sven Greb <code@svengreb.de>
+ * Copyright (C) 2017-present Arctic Ice Studio <development@arcticicestudio.com>
+ * Copyright (C) 2017-present Sven Greb <development@svengreb.de>
  *
  * Project:    Nord highlight.js
  * Repository: https://github.com/arcticicestudio/nord-highlightjs
@@ -8,24 +8,24 @@
  */
 
 /**
-* Configuration for stylelint.
-*
-* @see https://stylelint.io
-* @see https://stylelint.io/user-guide/rules
-* @see https://github.com/stylelint/stylelint-config-standard
-*/
+ * Configuration for stylelint.
+ *
+ * @see https://stylelint.io
+ * @see https://stylelint.io/user-guide/rules
+ * @see https://github.com/stylelint/stylelint-config-standard
+ */
 module.exports = {
-  "extends": "stylelint-config-standard",
-  "rules": {
+  extends: "stylelint-config-standard",
+  rules: {
     "color-hex-case": "lower",
     "comment-empty-line-before": null,
     "selector-pseudo-element-colon-notation": "single",
-    "at-rule-empty-line-before": [ "always", {
-      except: [
-        "blockless-after-same-name-blockless",
-        "first-nested"
-      ],
-      ignoreAtRules: ["after-comment", "import"]
-    }]
+    "at-rule-empty-line-before": [
+      "always",
+      {
+        except: ["blockless-after-same-name-blockless", "first-nested"],
+        ignoreAtRules: ["after-comment", "import"]
+      }
+    ]
   }
-}
+};
